f80990ca4278919e0de38926b8012b607c9f273f
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-14 20:39:39 -0400

    Fixed server

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
