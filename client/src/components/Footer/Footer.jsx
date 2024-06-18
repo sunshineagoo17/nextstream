1983777ca6375ba5a423933c31aa629df465c3f1
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-18 14:59:37 -0400

    Fixed footer desktop breakpoint

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,22 +1,27 @@
-import React from "react";
 import "./Footer.scss";
 import logoMark from "../../assets/images/logo-wordmark-wh.png";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-        <div className="footer__copy-container">
-          <div className="footer__copyright">© 2024</div>
-          <div className="footer__privacy-policy">Privacy Policy</div>
-          <div className="footer__tac">Terms and Conditions</div>
-        </div>
+
+      <img className="footer__logo-wordmark" alt="NextStream Logo" src={logoMark} />
+      
+      <div className="footer__copy-container">
+        <div className="footer__copyright">© 2024</div>
+        <div className="footer__privacy-policy">Privacy Policy</div>
+        <div className="footer__tac">Terms and Conditions</div>
+      </div>
+      
+      <div className="footer__btn-container">
         <button className="footer__btn-register">
           <div className="footer__register">Register</div>
         </button>
         <button className="footer__btn-contact">
           <div className="footer__contact">Contact Us</div>
         </button>
-        <img className="footer__logo-wordmark" alt="NextStream Logo" src={logoMark} />
+      </div>
+    
     </footer>
   );
 };
