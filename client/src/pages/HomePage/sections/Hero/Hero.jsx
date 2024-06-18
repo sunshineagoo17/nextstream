018ccd09bf1b0f4d3cf7aa27a70e3ad2f4f4a5b8
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-18 15:31:20 -0400

    Fixed Hero code

--- client/src/pages/HomePage/sections/Hero/Hero.jsx
+++ client/src/pages/HomePage/sections/Hero/Hero.jsx
@@ -5,25 +5,26 @@ import "./Hero.scss";
 export const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero__logo-container">
-        <img className="hero__logo" alt="NextStream Logo" src={NextStreamLogo} />
-      </div>
+
       <div className="hero__copy-container">
         <div className="hero__title">
           Can’t Stream?
           <br />
           Try NextStream!
         </div>
-        <div className="hero__copy">
-          <p className="hero__subtitle">
-              Discover where your favourite movies and shows are streaming, all in one place. Enjoy personalized
-              recommendations and easy scheduling to enhance your viewing experience.
-          </p>
-          <button className="hero__btn-container">
-            <div className="hero__btn">Start Watching</div>
-          </button>
-        </div>
+        <p className="hero__subtitle">
+            Discover where your favourite movies and shows are streaming, all in one place. Enjoy personalized
+            recommendations and easy scheduling to enhance your viewing experience.
+        </p>
+        <button className="hero__btn-container">
+          <div className="hero__btn">Start Watching</div>
+        </button>
       </div>
+
+      <div className="hero__logo-container">
+        <img className="hero__logo" alt="NextStream Logo" src={NextStreamLogo} />
+      </div>
+      
     </div>
   );
 };
