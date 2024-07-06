3d5d19c513f55f56a33707c1f9e19e118b058a3a
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-06 00:51:56 -0400

    Styled subscription status

--- client/src/pages/ProfilePage/sections/SubscriptionStatus/SubscriptionStatus.jsx
+++ client/src/pages/ProfilePage/sections/SubscriptionStatus/SubscriptionStatus.jsx
@@ -19,17 +19,17 @@ const SubscriptionStatus = () => {
         <div className="subscription-status__title">Subscription Status</div>
         <div className="subscription-status__active">
           <div className="subscription-status__select" onClick={toggleActiveStatus}>
-            <div className={`subscription-status__checkbox ${isActive ? 'subscription-status__active' : ''}`}>
+            <div className={`subscription-status__checkbox ${isActive ? 'subscription-status__checkbox--active' : ''}`}>
               {isActive && <div className="subscription-status__check">✔</div>}
             </div>
           </div>
-          <div className="subscription-status__active">
+          <div className="subscription-status__box">
             <div className="subscription-status__input">Active</div>
           </div>
         </div>
       </div>
       <div
-        className={`subscription-status__delete-account ${deleteAccount ? 'subscription-status__delete' : ''}`}
+        className={`subscription-status__delete-account ${deleteAccount ? 'subscription-status__delete-account--delete' : ''}`}
         onClick={handleDeleteAccount}
       >
         <div className="subscription-status__delete-txt">Delete Account</div>
