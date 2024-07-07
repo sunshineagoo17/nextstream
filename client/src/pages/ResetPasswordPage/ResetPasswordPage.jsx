5289817555acfef492c0e13d4a1c9e15fd5c39bd
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-06 22:22:00 -0400

    Styled the reset password page

--- client/src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ client/src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -10,8 +10,10 @@ export const ResetPasswordPage = () => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [validationError, setValidationError] = useState({});
   const navigate = useNavigate();
   const location = useLocation();
   const token = new URLSearchParams(location.search).get('token');
@@ -24,20 +26,41 @@ export const ResetPasswordPage = () => {
     setConfirmPasswordVisible(!confirmPasswordVisible);
   };
 
+  const handleInputChange = (e, setState) => {
+    setState(e.target.value);
+    setValidationError((prev) => ({
+      ...prev,
+      [e.target.name]: '',
+    }));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (newPassword !== confirmPassword) {
-      setMessage('Passwords do not match.');
+    let errors = {};
+    if (!newPassword) {
+      errors.newPassword = 'Please fill out this field.';
+    } else if (newPassword.length < 8) {
+      errors.newPassword = 'Password must be 8+ chars.';
+    }
+    if (!confirmPassword) {
+      errors.confirmPassword = 'Please fill out this field.';
+    } else if (newPassword !== confirmPassword) {
+      errors.confirmPassword = 'Passwords do not match.';
+    }
+    setValidationError(errors);
+    if (Object.keys(errors).length > 0) {
       return;
     }
     try {
       const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/password-reset/reset-password`, { token, newPassword });
       setMessage(response.data.message);
+      setMessageType(response.data.success ? 'success' : 'error');
       if (response.data.success) {
-        navigate('/login'); // Redirect to login page after successful reset
+        navigate('/login');
       }
     } catch (error) {
       setMessage('Error resetting password. Please try again.');
+      setMessageType('error');
     }
   };
 
@@ -46,12 +69,13 @@ export const ResetPasswordPage = () => {
       <div className="reset-password__container">
         <div className="reset-password__content-card">
           <h1 className="reset-password__title">Reset Password</h1>
-          <form onSubmit={handleSubmit}>
+          <form className="reset-password__form" onSubmit={handleSubmit}>
             <div className="reset-password__input-group reset-password__input-group--password">
               <input
                 type={passwordVisible ? "text" : "password"}
                 value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
+                name="newPassword"
+                onChange={(e) => handleInputChange(e, setNewPassword)}
                 required
                 className="reset-password__input"
                 placeholder=" "
@@ -66,11 +90,15 @@ export const ResetPasswordPage = () => {
                 <img src={passwordVisible ? HideIcon : ShowIcon} alt="Toggle visibility" className="reset-password__password-toggle-icon" />
               </button>
             </div>
+            {validationError.newPassword && (
+              <div className="reset-password__validation-error">{validationError.newPassword}</div>
+            )}
             <div className="reset-password__input-group reset-password__input-group--password">
               <input
                 type={confirmPasswordVisible ? "text" : "password"}
                 value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                name="confirmPassword"
+                onChange={(e) => handleInputChange(e, setConfirmPassword)}
                 required
                 className="reset-password__input"
                 placeholder=" "
@@ -85,9 +113,16 @@ export const ResetPasswordPage = () => {
                 <img src={confirmPasswordVisible ? HideIcon : ShowIcon} alt="Toggle visibility" className="reset-password__password-toggle-icon" />
               </button>
             </div>
+            {validationError.confirmPassword && (
+              <div className="reset-password__validation-error">{validationError.confirmPassword}</div>
+            )}
             <button type="submit" className="reset-password__button">Reset Password</button>
           </form>
-          {message && <p className={`reset-password__message ${message.includes('Error') ? 'reset-password__message--error' : 'reset-password__message--success'}`}>{message}</p>}
+          {message && (
+            <div className={`reset-password__message ${messageType === 'error' ? 'reset-password__message--error' : 'reset-password__message--success'}`}>
+              {message}
+            </div>
+          )}
         </div>
       </div>
       <div className="reset-password__background">
