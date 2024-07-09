e46c9e10a689ff4f6e036603c9ebbf74f0b53f6f
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-09 11:42:54 -0400

    Fixed hover menu

--- client/src/hooks/useMenuLinks.jsx
+++ client/src/hooks/useMenuLinks.jsx
@@ -6,94 +6,28 @@ const useMenuLinks = () => {
   const location = useLocation();
   const { isAuthenticated, userId: authUserId } = useContext(AuthContext);
 
-  const authenticatedLinksSet1 = [
-    { name: "Stream Locator", path: "/stream-locator" },
-    { name: "Top Picks", path: "/top-picks" },
+  const authenticatedLinks = [
+    { name: "Stream Locator", path: `/stream-locator/${authUserId}` },
+    { name: "Top Picks", path: `/top-picks/${authUserId}` },
     { name: "Calendar", path: `/calendar/${authUserId}` },
     { name: "Profile", path: `/profile/${authUserId}` }
   ];
 
-  const authenticatedLinksSet2 = [
-    { name: "Stream Locator", path: "/stream-locator" },
-    { name: "Top Picks", path: "/top-picks" },
-    { name: "Calendar", path: `/calendar/${authUserId}` },
-  ];
-
   const unauthenticatedLinks = [
+    { name: "Register", path: "/register" },
+    { name: "Login", path: "/login" },
     { name: "Terms", path: "/terms" },
     { name: "Privacy Policy", path: "/privacy-policy" }
   ];
 
-  const unauthenticatedHomeLink = [
-    { name: "Home", path: "/" }
-  ];
-
-  const unauthenticatedRegisterLink = [
-    { name: "Register", path: "/register" }
-  ];
-
-  const unauthenticatedLoginLink = [
-    { name: "Register", path: "/login" }
-  ];
-
-  const loginRequiredPath = '/login-required';
-
-  const navigateToLoginRequired = () => {
-    window.location.href = loginRequiredPath; 
-  };
-
   const getMenuLinks = (path) => {
+    const homeLink = { name: "Home", path: "/" };
     if (isAuthenticated) {
-      if (path.startsWith(`/profile/${authUserId}`)) {
-        return [
-          { name: "Home", path: "/" },
-          ...authenticatedLinksSet2
-        ];
-      } else {
-        // Handle other authenticated routes
-        return authenticatedLinksSet1;
-      }
+      const filteredAuthenticatedLinks = authenticatedLinks.filter(link => link.path !== path);
+      return path !== "/" ? [homeLink, ...filteredAuthenticatedLinks] : filteredAuthenticatedLinks;
     } else {
-      switch (path) {
-        case "/privacy-policy":
-        case "/terms":
-        case "/search":
-          return [
-            ...unauthenticatedHomeLink,
-            ...authenticatedLinksSet1,
-            ...unauthenticatedRegisterLink,
-          ];
-        case "/register":
-          return [
-            ...unauthenticatedHomeLink,
-            ...unauthenticatedLoginLink,
-            ...unauthenticatedLinks
-          ];
-        case "/login-required":
-          return [
-            ...unauthenticatedHomeLink,
-            ...unauthenticatedRegisterLink,
-            ...unauthenticatedLinks
-          ];
-        case "/login":
-          return [
-            ...unauthenticatedHomeLink,
-            ...unauthenticatedRegisterLink,
-            ...unauthenticatedLinks
-          ];
-        case "/":
-          return [
-            ...unauthenticatedRegisterLink,
-            ...authenticatedLinksSet1
-          ];
-        default:
-          // Redirect to login-required page for authenticated routes if not logged in
-          if (authenticatedLinksSet1.some(link => link.path === path)) {
-            navigateToLoginRequired();
-            return [];
-          }
-          return unauthenticatedLinks;
-      }
+      const filteredUnauthenticatedLinks = unauthenticatedLinks.filter(link => link.path !== path);
+      return path !== "/" ? [homeLink, ...filteredUnauthenticatedLinks] : filteredUnauthenticatedLinks;
     }
   };
 
