ef568d8d794034a7bf989b71bc85df4f65951de0
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-13 19:48:58 -0400

    Added usercore for media cards in top picks page

---
+++ client/src/pages/TopPicksPage/sections/CircleRating/CircleRating.jsx
@@ -0,0 +1,38 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './CircleRating.scss';
+
+const CircleRating = ({ rating }) => {
+  const percentage = Math.min(Math.max(rating, 0), 100);
+
+  const getColor = (percent) => {
+    if (percent >= 75) {
+      return '#012E71';
+    } else if (percent >= 50) {
+      return '#0045B6';
+    } else if (percent >= 25) {
+      return '#4092FF';
+    } else {
+      return '#00C9FF';
+    }
+  };
+
+  return (
+    <div className="circle-rating-container">
+      <div className="circle-rating">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            pathColor: getColor(percentage),
+            textColor: '#012E71',
+            trailColor: '#EEF4FC',
+          })}
+        />
+      </div>
+      <span className="tooltip-text">User Score:</span>
+    </div>
+  );
+};
+
+export default CircleRating;
--- client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
+++ client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
@@ -1,11 +1,15 @@
-const MediaCard = ({ media, handlers }) => {
-  const imageUrl = `https://image.tmdb.org/t/p/w500${media.poster_path}`; 
+import CircleRating from '../CircleRating/CircleRating';
+import './MediaCard.scss';
 
+const MediaCard = ({ media, handlers }) => {
   return (
     <div className="media-card" {...handlers}>
-      <img src={imageUrl} alt={media.title || media.name} />
-      <h2>{media.title || media.name}</h2>
-      <p>{media.overview}</p>
+      <img src={`https://image.tmdb.org/t/p/w500${media.poster_path}`} alt={media.title || media.name} className="media-card__image"/>
+      <div className="media-card__details">
+        <h2 className="media-card__title">{media.title || media.name}</h2>
+        <CircleRating rating={media.vote_average * 10} />
+        <p className="media-card__overview">{media.overview}</p>
+      </div>
     </div>
   );
 };
