167b2cca9fe76a4934638dc9f9ce31a9c674666e
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-14 23:39:16 -0400

    Added links to the movies to TMDB resource

--- client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
+++ client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
@@ -3,12 +3,17 @@ import './MediaCard.scss';
 
 const MediaCard = ({ media, handlers }) => {
   const validRating = Math.min(Math.max(Math.round(media.vote_average * 10 * 100) / 100, 0), 100);
+  const tmdbUrl = `https://www.themoviedb.org/${media.media_type === 'tv' ? 'tv' : 'movie'}/${media.id}`;
 
   return (
     <div className="media-card" {...handlers}>
       <img src={`https://image.tmdb.org/t/p/w500${media.poster_path}`} alt={media.title || media.name} className="media-card__image"/>
       <div className="media-card__details">
-        <h2 className="media-card__title">{media.title || media.name}</h2>
+        <h2 className="media-card__title">
+          <a href={tmdbUrl} target="_blank" rel="noopener noreferrer" className="media-card__tmdb-link">
+            {media.title || media.name}
+          </a>
+        </h2>
         <CircleRating rating={validRating} />
         <p className="media-card__overview">{media.overview}</p>
       </div>
