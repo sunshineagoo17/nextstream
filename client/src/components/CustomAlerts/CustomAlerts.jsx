aa3cb4018c9dd4d1b6bd25d660f54a22dfdfb704
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-07-24 12:15:25 -0400

    Added icons to custom alerts

--- client/src/components/CustomAlerts/CustomAlerts.jsx
+++ client/src/components/CustomAlerts/CustomAlerts.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faInfoCircle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import './CustomAlerts.scss';
 
 const CustomAlerts = ({ message, type, onClose }) => {
@@ -7,10 +9,24 @@ const CustomAlerts = ({ message, type, onClose }) => {
     return () => clearTimeout(timer);
   }, [onClose]);
 
+  const getIcon = () => {
+    switch (type) {
+      case 'success':
+        return faCheckCircle;
+      case 'info':
+        return faInfoCircle;
+      case 'error':
+        return faExclamationCircle;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={`custom-alerts custom-alerts--${type}`}>
       <div className="custom-alerts__content">
-        <span>{message}</span>
+        <FontAwesomeIcon icon={getIcon()} className="custom-alerts__icon" />
+        <span className="custom-alerts__message">{message}</span>
         <button onClick={onClose} className="custom-alerts__close-btn">&times;</button>
       </div>
       <div className="custom-alerts__progress-bar"></div>
