2205710794815aced5ae7f37319a808520402240
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-08-24 21:49:31 -0400

    Added next view page link to top picks page

--- client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
+++ client/src/pages/TopPicksPage/sections/MediaCard/MediaCard.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilm, faTv, faSearch, faImage } from '@fortawesome/free-solid-svg-icons';
+import { faFilm, faTv, faImage } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../../../context/AuthContext/AuthContext';
 import CircleRating from '../CircleRating/CircleRating';
 import DefaultPoster from "../../../../assets/images/posternoimg-icon.png";
 import './MediaCard.scss';
@@ -9,18 +11,13 @@ const MediaCard = ({ media = {}, handlers }) => {
   const { media_type = 'unknown', vote_average, title, name, id, poster_path, overview } = media;
   const validRating = Math.min(Math.max(Math.round((vote_average || 0) * 10 * 100) / 100, 0), 100);
   const navigate = useNavigate();
+  const { userId } = useContext(AuthContext);
 
-  const handleSearchClick = () => {
-    const query = title || name;
-    const encodedQuery = encodeURIComponent(query);
-    navigate(`/search?q=${encodedQuery}`);
+  const handleMediaTypeClick = () => {
+    if (media_type === 'tv' || media_type === 'movie') {
+      navigate(`/nextview/${userId}/${media_type}/${id}`);
+    }
   };
-
-  const tmdbUrl = media_type === 'tv'
-    ? `https://www.themoviedb.org/tv/${id}`
-    : media_type === 'movie'
-    ? `https://www.themoviedb.org/movie/${id}`
-    : `https://www.themoviedb.org/collection/${id}`;
 
   const icon = media_type === 'tv'
     ? faTv
@@ -37,13 +34,10 @@ const MediaCard = ({ media = {}, handlers }) => {
       <img src={posterUrl} alt={title || name} className="media-card__image" />
       <div className="media-card__details">
         <h2 className="media-card__title">
-          <a href={tmdbUrl} target="_blank" rel="noopener noreferrer" className="media-card__tmdb-link">
             {title || name}
-          </a>
-          <a href={tmdbUrl} target="_blank" rel="noopener noreferrer" className="media-card__type-link">
+          <span onClick={handleMediaTypeClick} className="media-card__type-link">
             <FontAwesomeIcon icon={icon} className="media-card__type-icon" />
-          </a>
-          <FontAwesomeIcon icon={faSearch} className="media-card__search-icon" onClick={handleSearchClick} />
+          </span>
         </h2>
         <CircleRating rating={validRating} />
         <p className="media-card__overview">{overview}</p>
