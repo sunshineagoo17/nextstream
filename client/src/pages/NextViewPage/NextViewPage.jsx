2538b9b7ab2553f7e4998b411692c23aedb735a9
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-08-24 22:57:52 -0400

    Added title and copy to next view page

--- client/src/pages/NextViewPage/NextViewPage.jsx
+++ client/src/pages/NextViewPage/NextViewPage.jsx
@@ -228,6 +228,13 @@ const NextViewPage = () => {
                     onClose={closeAlert}
                 />
             )}
+            <div className="nextview-page__heading-container">
+                <h1 className="nextview-page__header">NextView</h1>
+                <p className="nextview-page__copy">
+                    <span className="nextview-page__gradient-subtitle">NextView</span>
+                    is a detailed media display page in your app. It shows all the essential information about a selected movie or TV show, including its title, description, rating, genres, and available streaming services. You can watch a trailer, add the media to your calendar, and interact by liking or disliking it. The page is designed to give users a comprehensive and interactive experience with their chosen media.
+                </p>
+            </div>
             <div className="nextview-page__content">
                 <h1 className="nextview-page__title">
                     {mediaData.title || mediaData.name}
