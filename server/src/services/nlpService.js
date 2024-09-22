8edd0bba79a19bfff6c29de20cac44fa1cd9a9fb
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-09-22 11:26:15 -0400

    Changed bot messages in nextstream bot

--- server/src/services/nlpService.js
+++ server/src/services/nlpService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0382a43a25831f618b356a73334975deec017cfc079fa63a89c5e50b4adea9de
-size 944286
+oid sha256:93d95c14ef5e57333879f0e8b056e31f0798cb74502f54d845460b603d70ede4
+size 944326
