ab617f3e58e3e9ab766de9cbe03defd114b0ca3f
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-10-07 15:13:33 -0400

    Added cloud mode to homepage

--- client/src/components/Header/sections/ThemeToggle/ThemeToggle.jsx
+++ client/src/components/Header/sections/ThemeToggle/ThemeToggle.jsx
@@ -6,33 +6,40 @@ import './ThemeToggle.scss';
 const ThemeToggle = () => {
   const [theme, setTheme] = useState('light');
 
+  // Check for saved theme in localStorage or default to 'light'
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', savedTheme);
     setTheme(savedTheme);
   }, []);
 
+  // Function to toggle between themes
   const toggleTheme = () => {
-    const newTheme = theme === 'light' 
-      ? 'dark' 
-      : theme === 'dark' 
-      ? 'cloud-mode'
-      : theme === 'cloud-mode'
-      ? 'trans-mode' 
-      : 'light';
-      
+    let newTheme = '';
+
+    if (theme === 'light') {
+      newTheme = 'dark';
+    } else if (theme === 'dark') {
+      newTheme = 'cloud-mode';
+    } else if (theme === 'cloud-mode') {
+      newTheme = 'trans-mode';
+    } else {
+      newTheme = 'light';  // Reset to light mode
+    }
+
     document.documentElement.setAttribute('data-theme', newTheme);
     setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
+    localStorage.setItem('theme', newTheme);  // Save the theme to localStorage
   };
 
   return (
     <button onClick={toggleTheme} className={`dark-mode-toggle ${theme}`}>
+      {/* Icon changes based on current theme */}
       {theme === 'dark' ? (
         <FontAwesomeIcon icon={faMoon} className="icon moon" />
       ) : theme === 'trans-mode' ? (
         <FontAwesomeIcon icon={faRainbow} className="icon rainbow" />
-      ) : theme === 'cloud-mode' ? (  
+      ) : theme === 'cloud-mode' ? (
         <FontAwesomeIcon icon={faCloud} className="icon cloud" />
       ) : (
         <FontAwesomeIcon icon={faSun} className="icon sun" />
