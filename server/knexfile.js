e0a4b63f31d164b348bf922138d5a4574b24e8a3
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-10-22 16:29:15 -0400

    Tweaked knexfiled

--- server/knexfile.js
+++ server/knexfile.js
@@ -31,7 +31,10 @@ module.exports = {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            port: process.env.DB_PORT || 3306, 
+            port: process.env.DB_PORT || 3306,
+            ssl: {
+                rejectUnauthorized: false,
+            },
         },
         debug: false,
     },
