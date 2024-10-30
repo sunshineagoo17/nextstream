be506455a80ee674218c5c86e4e5f7caca5e27d7
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-10-29 21:10:02 -0400

    Update push notification service to load Firebase config from environment variable

--- server/src/services/pushNotificationService.js
+++ server/src/services/pushNotificationService.js
@@ -3,7 +3,7 @@ const moment = require('moment-timezone');
 const path = require('path');
 
 if (!admin.apps.length) {
-    const serviceAccount = require(path.join(__dirname, '../creds/nextstream-serviceAccountKey.json'));
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
